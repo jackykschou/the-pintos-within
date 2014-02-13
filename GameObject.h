c066a4d9dccb863fba643c3ee5d1bfb11341cac4
KeeganStandifer <keegan@austin.rr.com>
2014-02-13 13:21:34 -0600

    Fleshed out GameObject skeleton

--- GameObject.h
+++ GameObject.h
@@ -1,6 +1,17 @@
 class GameObject
 {
 public:
+
+	bool active;
+	//AudioSource audio;
+	//Collider collider;
+	//Vector3 constantForce;
+	int layer;
+	//Renderer renderer;
+	//Rigidbody rigidbody;
+	string tag;
+	//Transform transform;
+
 	GameObject();
 
 	~GameObject();
