75115367f9037e3fc6cc00280c99472fdf67aa50
Joe Vennix <joevennix@gmail.com>
2014-02-20 17:48:35 -0600

    Work on AudioManager

--- AudioManager.cpp
+++ AudioManager.cpp
@@ -1,5 +1,23 @@
 #include "AudioManager.h"
 
 void AudioManager::initialize() {
+	SDL_Init(SDL_INIT_AUDIO);
+	if (Mix_OpenAudio(AUDIO_RATE, AUDIO_FORMAT, AUDIO_CHANNELS, AUDIO_BUFFERS)) {
+	    throw("Unable to open audio channel.");
+	}
+}
 
+// returns an audioFileKey that references the loaded resource
+AudioFile AudioManager::loadAudioFile(const char *file) {
+	_loadedFiles.push_back(Mix_LoadWAV(file));
+	return _loadedFiles.size() - 1;
+}
+
+// returns a channel integer that can be used to stop the sound
+AudioChannel AudioManager::play2DSound(AudioFile file, int loops) {
+	return Mix_PlayChannel(-1, _loadedFiles[file], loops);
+}
+
+void AudioManager::stopChannel(AudioChannel channel) {
+	Mix_HaltChannel(channel);
 }
--- AudioManager.h
+++ AudioManager.h
@@ -1,9 +1,27 @@
+#ifndef __AudioManager_h_
+#define __AudioManager_h_
+
 #include <SDL/SDL.h>
 #include <SDL/SDL_mixer.h>
-
 #include "common.h"
+
+#define AUDIO_RATE 22050
+#define AUDIO_FORMAT AUDIO_S16
+#define AUDIO_CHANNELS 2
+#define AUDIO_BUFFERS 4096
+
+typedef unsigned int AudioFile;
+typedef int AudioChannel;
 
 class AudioManager : public Singleton<AudioManager> {
   public:
   	void initialize();
+  	AudioFile    loadAudioFile(const char* file);
+  	AudioChannel play2DSound(AudioFile, int loops);
+  	void stopChannel(AudioChannel);
+
+  private:
+  	std::vector<Mix_Chunk*> _loadedFiles;
 };
+
+#endif
