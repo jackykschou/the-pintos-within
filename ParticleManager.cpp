0c52af133cc616ea1756388db0f5ba8c9fd89656
Michael Passaloukos <michael.passaloukos@utexas.edu>
2014-03-20 18:41:53 -0500

    ParticleManager possibly ready for integration.

--- ParticleManager.cpp
+++ ParticleManager.cpp
@@ -5,35 +5,59 @@ ParticleManager::ParticleManager(){
 }
 ParticleManager::~ParticleManager(){
 }
-void ParticleManager::Emit(Ogre::Vector3 origin, Ogre::Vector3 direction, std::string particleSystemName){
-  //Ogre::SceneManager* sceneManager=SceneManager::instance()->current_scene->manager;
-  extern Ogre::SceneManager* sceneManager;
 
+Ogre::ParticleSystem* ParticleManager::Emit(std::string particleSystemName,Ogre::Vector3 origin,
+    Ogre::Vector3 direction){
+  Ogre::SceneManager* sceneManager=SceneManager::instance()->current_scene->manager;
+  //extern Ogre::SceneManager* sceneManager;
+  Ogre::SceneNode* node=sceneManager->createSceneNode();
+  sceneManager->getRootSceneNode()->addChild(node);
+  positionAndDirectSceneNode(node,origin,direction);
+  return Emit(particleSystemName,node);
+}
+
+Ogre::ParticleSystem* ParticleManager::Emit(std::string particleSystemName,Ogre::Vector3 origin,
+    Ogre::Vector3 direction,Ogre::SceneNode* parentNode){
+  Ogre::SceneNode* node{parentNode->createChildSceneNode()};
+  positionAndDirectSceneNode(node,origin,direction);
+  return Emit(particleSystemName,node);
+}
+
+Ogre::ParticleSystem* ParticleManager::Emit(std::string particleSystemName,Ogre::SceneNode* node){
+  Ogre::ParticleSystem* system{lookUpSystem(particleSystemName)};
+  if(system->isInScene()){
+    Ogre::SceneNode* formerNode = system->getParentSceneNode();
+    system->detachFromParent();
+    formerNode->getParentSceneNode()->removeAndDestroyChild(formerNode->getName());
+  }
+  node->attachObject(system);
+  system->getEmitter(0)->setEnabled(true);
+  return system;
+}
+
+Ogre::ParticleSystem* ParticleManager::lookUpSystem(std::string particleSystemName){
   auto range=_systems.equal_range(particleSystemName);
   Ogre::ParticleSystem* system{nullptr};
   int howMany{1};
   for(auto systemElement=range.first;systemElement!=range.second;++systemElement){
-    if((*systemElement).second->getNumParticles()==0){
+    //if((*systemElement).second->getNumParticles()==0){
+    if(!(*systemElement).second->getEmitter(0)->getEnabled()){
       system=(*systemElement).second.get();
       break;
     }
     ++howMany;
   }
-  Ogre::SceneNode* node;
-  if(system!=nullptr){
-    node=system->getParentSceneNode();
-    Ogre::SceneNode* parent=node->getParentSceneNode();
-    //system->setEmitting(false);
-    system->detachFromParent();
-    parent->removeChild(node);
-    node->attachObject(system);
-    parent->addChild(node);
-    //system->setEmitting(true);
-  }else{
+  std::cout<<"How many: "<<howMany<<std::endl;
+  if(system==nullptr){
+    Ogre::SceneManager* sceneManager=SceneManager::instance()->current_scene->manager;
+    //extern Ogre::SceneManager* sceneManager;
     system=sceneManager->createParticleSystem(particleSystemName+std::to_string(howMany),particleSystemName);
     _systems.insert(std::make_pair(particleSystemName,std::unique_ptr<Ogre::ParticleSystem>{system}));
-    node=sceneManager->createSceneNode();
-    node->attachObject(system);
-    sceneManager->getRootSceneNode()->addChild(node);
   }
+  return system;
+}
+
+void ParticleManager::positionAndDirectSceneNode(Ogre::SceneNode* node,Ogre::Vector3 origin, Ogre::Vector3 direction){
+  node->setPosition(origin);
+  node->setDirection(direction);
 }
--- ParticleManager.h
+++ ParticleManager.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <map>
 #include <OgreParticleSystem.h>
+#include <OgreParticleEmitter.h>
 #include <OgreVector3.h>
 #include "Singleton.h"
 
@@ -11,8 +12,14 @@ class ParticleManager:public Singleton<ParticleManager>{
   public:
     ParticleManager();
     virtual ~ParticleManager();
-    void Emit(Ogre::Vector3 origin, Ogre::Vector3 direction, std::string particleSystemName);
+    Ogre::ParticleSystem* Emit(std::string particleSystemName,Ogre::Vector3 origin,
+        Ogre::Vector3 direction);
+    Ogre::ParticleSystem* Emit(std::string particleSystemName,Ogre::Vector3 origin,
+        Ogre::Vector3 direction,Ogre::SceneNode* parentNode);
+    Ogre::ParticleSystem* Emit(std::string particleSystemName,Ogre::SceneNode* node);
   private:
     std::multimap<std::string,std::unique_ptr<Ogre::ParticleSystem>> _systems;
+    Ogre::ParticleSystem* lookUpSystem(std::string particleSystemName);
+    void positionAndDirectSceneNode(Ogre::SceneNode* node,Ogre::Vector3 origin, Ogre::Vector3 direction);
 };
 #endif
